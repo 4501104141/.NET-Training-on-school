7e920656f2a82382c554e7f098ee746a5356f287
Minh <4501104141@student.hcmue.edu.vn>
2022-09-07 11:54:06 +0700

    Add project files.

---
+++ LAP01/Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LAP01.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Calculate(double a1 = 0, double b = 0, char op = '+')
+        {
+            switch (op)
+            {
+                case '+':
+                    ViewBag.KetQua = a1 + b;
+
+                    break;
+                case '-':
+                    ViewBag.KetQua = a1 - b;
+                    break;
+                case 'x':
+                    ViewBag.KetQua = a1 * b;
+
+                    break;
+                case ':':
+                    ViewBag.KetQua = a1 / b;
+
+                    break;
+
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ LAP01/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using LAP01.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAP01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public string Actiontest()
+        {
+            return "okela";
+        }
+        public ViewResult ActionIndex()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.LoiChao = hour < 12 ? "Chào buổi sáng" : "Chào buổi chiều";
+            return View("MyView");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
