3573b09d30063df07b40fdb9f27dc466140296ff
Minh <4501104141@student.hcmue.edu.vn>
2022-09-08 10:14:10 +0700

    Edit and delete🛠️

--- LAP01/Controllers/ProductController.cs
+++ LAP01/Controllers/ProductController.cs
@@ -17,6 +17,7 @@ namespace LAP01.Controllers
         {
             return View();
         }
+        [HttpPost]
         public IActionResult Create3([Bind("ID", "Name", "Price")] Product product)
         {
             //thêm vào danh sách
@@ -24,5 +25,44 @@ namespace LAP01.Controllers
             //gọi hiển thị danh sách
             return RedirectToAction("ShowAll");
         }
+
+        public IActionResult Edit(int id)
+        {
+            Product p = products.SingleOrDefault(q => q.ID == id);
+            if (p != null) //tìm thấy
+            {
+                return View(p);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        [HttpPost]
+        public IActionResult Edit33(int id, [Bind("Id", "Name", "Price")] Product product)
+        {
+            //Sửa vào danh sách
+            Product p = products.SingleOrDefault(q => q.ID == id);
+            if (p != null) //tìm thấy
+            {
+                p.Name = product.Name;
+                p.Price = product.Price;
+            }
+            //gọi hiển thị danh sách
+            return RedirectToAction("ShowAll");
+        }
+
+        public IActionResult Delete33(int id)
+        {
+            //tìm Product cần xóa (dùng LINQ)
+            Product p = products.SingleOrDefault(q => q.ID == id);
+            if (p != null) //tìm thấy
+            {
+                products.Remove(p);
+            }
+            //gọi hiển thị danh sách
+            return RedirectToAction("ShowAll");
+        }
+
     }
 }
